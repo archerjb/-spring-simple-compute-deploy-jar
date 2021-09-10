e2a60dd21844669f0febfc906e4297cc94a35947
Josh Archer <jarcher@greenriver.edu>
2021-09-10 13:58:44 -0700

    Adding initial files.

---
+++ src/main/java/com/example/springsimplecomputedeployjar/SpringSimpleComputeDeployJarApplication.java
@@ -0,0 +1,15 @@
+package com.example.springsimplecomputedeployjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleComputeDeployJarApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringSimpleComputeDeployJarApplication.class, args);
+    }
+
+}
